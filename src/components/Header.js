33593f9a56666170fdc0de5f5d243d05027a6199
Navneet Sidhu <sidhunavneet52@gmail.com>
2019-08-08 20:54:15 -0400

    Edited h2 tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ import logo from '../assets/images/logo.svg';
 const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo} alt="" /></span>
-        <h1>Stellar</h1>
-        <h2>Masters Project</h2>
-        <p>Aug 2019<br />
+        <h1>Interactive Educational Tool for Fitness Trackers</h1>
+        <h2>Know about your Fitness Tracker's Privacy Policies?? Think Again!</h2>
+        <p><br />
         design by <a href="https://html5up.net">HTML5 UP</a>.</p>
     </header>
 )
